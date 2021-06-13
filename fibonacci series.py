19b2d80965e2dd6ddbee066a7e639fbebc522a08
Borntorule007 <84105603+Borntorule007@users.noreply.github.com>
2021-06-14 02:08:34 +0530

    Update fibonacci series.py

--- fibonacci series.py
+++ fibonacci series.py
@@ -1,3 +1,4 @@
+# Without Function
 a = 0
 b = 1
 x=int(input("Enter the number"))
@@ -8,3 +9,26 @@ for i in range(2,x):
     print(c)
     a=b
     b=c
+    print(c)
+    
+# With Function
+
+def fib(n):
+
+    a = 0
+    b = 1
+
+    if n == 1:
+        print(a)
+
+    else:
+        print(a)
+        print(b)
+
+    for i in range(2,n):
+        c = a + b
+        a = b
+        b = c
+        print(a+b)
+
+fib(100)
